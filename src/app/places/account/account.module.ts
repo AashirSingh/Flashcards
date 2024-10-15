72e62b68627ba171c219cb71e3ef5061ec7c9cea
AashirSingh <aashirsingh@gmail.com>
2024-10-15 15:03:41 -0700

    Deleted edit number component

--- src/app/places/account/account.module.ts
+++ src/app/places/account/account.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { AccountPageRoutingModule } from './account-routing.module';
 import { AccountPage } from './account.page';
+
 import { ProfilePictureComponent } from 'src/app/places/account/profile-picture/profile-picture.component';
 import { ProfileListComponent } from 'src/app/places/account/profile-list/profile-list.component'
 import { EditNameComponent,NameModalContent } from 'src/app/places/account/edit-name/edit-name.component';
-import { EditNumberComponent, NumberModalContent } from 'src/app/places/account/edit-number/edit-number.component';
 
 @NgModule({
   imports: [
@@ -16,8 +16,8 @@ import { EditNumberComponent, NumberModalContent } from 'src/app/places/account/
     IonicModule,
     AccountPageRoutingModule
   ],
-  declarations:[AccountPage, ProfilePictureComponent, ProfileListComponent, EditNameComponent, NameModalContent, EditNumberComponent, NumberModalContent],
-  exports: [ProfilePictureComponent, ProfileListComponent, EditNameComponent,  EditNumberComponent]
+  declarations: [AccountPage, ProfilePictureComponent, ProfileListComponent, EditNameComponent,NameModalContent],
+  exports: [ProfilePictureComponent, ProfileListComponent, EditNameComponent]
   
 })
 export class AccountPageModule {}
